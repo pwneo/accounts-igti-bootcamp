6b3c3b2a7cf1f602b216b9715d170acf2c13659c
Paulo W. de A. Ferreira <pauloweskley.dev@gmail.com>
2020-09-04 15:11:52 -0300

    Ajustes

--- repositories/account.repository.js
+++ repositories/account.repository.js
@@ -2,14 +2,13 @@ import mongoose from 'mongoose';
 import {accountModel} from '../models/account.model.js';
 
 export const db = {
-    url: `mongodb+srv://pwneo:puruca2009@cluster0.vziki.mongodb.net/bank?retryWrites=true&w=majority`,
     mongoose: mongoose,
     account: accountModel(mongoose)
 };
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(db.url, {
+        await mongoose.connect(`mongodb+srv://pwneo:puruca2009@cluster0.vziki.mongodb.net/bank?retryWrites=true&w=majority`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
